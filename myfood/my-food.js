18baeb2dded120ec7dd4f92a4a8bc6c3acd47b2d
Ibrahima <bah.ibrahima.pro@hotmail.com>
2023-07-28 12:59:11 +0200

    save

--- myfood/my-food.js
+++ myfood/my-food.js
@@ -9,8 +9,11 @@ let travel = document.querySelector(".tagz")
 let tags = document.querySelectorAll(".tagz")
 let flchgauche = document.querySelector(".gvoche")
 let flchdroite = document.querySelector(".drvoite")
-let slides = document.querySelectorAll()
-
+let slides = document.querySelectorAll(".imagese")
+console.log(slides)
+let currentIndex = 0;
+let gauche = document.querySelector("#gauche");
+let droite = document.querySelector('#droite');
 
 
 
@@ -106,3 +109,42 @@ tags.forEach(function (tag) {
         }
     });
 })
+
+
+function afficherImage(numero) {
+    for (i of [0, 1, 2]) {
+        if (i == numero) {
+            slides[i].style.display = "block"
+        } else {
+            slides[i].style.display = "none"
+        }
+    }
+}
+droite.addEventListener("click", function () {
+    if (currentIndex == 2) {
+        currentIndex = 0
+        afficherImage(currentIndex)
+    } else {
+        currentIndex = currentIndex + 1
+        afficherImage(currentIndex)
+    }
+    // currentIndex = currentIndex + 1
+    // afficherImage(currentIndex)
+    // currentIndex += 1
+    // currentIndex++
+
+})
+gauche.addEventListener("click", function () {
+
+    if (currentIndex == 0) {
+        currentIndex = 2
+        afficherImage(currentIndex)
+    } else {
+        currentIndex = currentIndex - 1
+        afficherImage(currentIndex)
+    }
+})
+
+afficherImage(currentIndex)
+// if currentIndex = 2
+// else afficherImage(currentIndex)
